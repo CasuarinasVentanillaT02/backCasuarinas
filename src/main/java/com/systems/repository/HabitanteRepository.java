9dfe12bef94529c6a634774f0f5bc77b5f9785ee
CasuarinasVentanillaT02 <casuarinasventanillat02@gmail.com>
2025-04-19 12:31:04 -0500

    DFV0019

--- src/main/java/com/systems/repository/HabitanteRepository.java
+++ src/main/java/com/systems/repository/HabitanteRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface HabitanteRepository extends JpaRepository<HabitantesEntity,Long> {
     Page<HabitantesEntity> findByNuDocumento(String nuDocumento, Pageable pageable);
-    @Query(value = "SELECT * FROM mantenimiento.tb_habitantes h WHERE CONCAT(h.de_apellidos, h.de_nombres) LIKE %:parametro%", nativeQuery = true)
+    @Query(value = "SELECT * FROM mantenimiento.tb_habitantes h WHERE upper(CONCAT(h.de_apellidos, h.de_nombres)) LIKE %upper(:parametro)%", nativeQuery = true)
     Page<HabitantesEntity> findByApellidosAndNombresCombinedNative(String parametro, Pageable pageable);
 }
 
