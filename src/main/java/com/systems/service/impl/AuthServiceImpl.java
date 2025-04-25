c4d1445b61ec3e6c47b1b9066a82bafb78e3be17
CasuarinasVentanillaT02 <casuarinasventanillat02@gmail.com>
2025-04-25 09:40:34 -0500

    DFV0024

--- src/main/java/com/systems/service/impl/AuthServiceImpl.java
+++ src/main/java/com/systems/service/impl/AuthServiceImpl.java
@@ -3,6 +3,7 @@ package com.systems.service.impl;
 import com.systems.dto.LoginRequest;
 import com.systems.dto.TokenResponse;
 import com.systems.dto.UserResponse;
+import com.systems.exceptions.CustomException;
 import com.systems.repository.AuthRepository;
 import com.systems.security.JwtUtils;
 import com.systems.service.AuthService;
@@ -27,35 +28,46 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public TokenResponse login(LoginRequest loginRequest) {
-        // Obtenemos el resultado de la consulta
-        List<Object[]> userArray = authRepository.validateLogin(loginRequest.deUsuario(), loginRequest.deClave());
-
-        // Aseguramos que hay suficientes datos en el array
-        Object[] user = userArray.get(0);
-
-        // Convertimos Object[] en UserResponse de manera segura
-        UserResponse userResponse = new UserResponse(
-                (Integer) user[0], // aio_nu_result
-                (String) user[1], // aso_de_result
-                (Integer) user[2], // aio_id_usuario
-                (String) user[3], // aso_de_alias
-                (String) user[4], // aso_de_rol
-                (Integer) user[5], // aio_id_habitante
-                (String) user[6] // aso_de_habitante
-        );
-
-        var jwtToken = "";
-        var refreshToken = "";
-
-        if (userResponse.aioNuResult() == 1) {
-            jwtToken = jwtService.generateToken(userResponse);
-            refreshToken = jwtService.generateRefreshToken(userResponse);
-        }//else{
+        
+        try{
+            // Obtenemos el resultado de la consulta
+            List<Object[]> userArray = authRepository.validateLogin(loginRequest.deUsuario(), loginRequest.deClave());
             
-            //throw new ResponseStatusException(HttpStatus.FORBIDDEN, userResponse.asoDeResult());
-        //}
+            // Aseguramos que hay suficientes datos en el array
+            if (userArray.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Usuario/Clave incorrecta");
+            }
+            
+            // Aseguramos que hay suficientes datos en el array
+            Object[] user = userArray.get(0);
+            
+            // Convertimos Object[] en UserResponse de manera segura
+            UserResponse userResponse = new UserResponse(
+                    (Integer) user[0], // aio_nu_result
+                    (String) user[1], // aso_de_result
+                    (Integer) user[2], // aio_id_usuario
+                    (String) user[3], // aso_de_alias
+                    (String) user[4], // aso_de_rol
+                    (Integer) user[5], // aio_id_habitante
+                    (String) user[6] // aso_de_habitante
+            );
 
-        return new TokenResponse(200,userResponse.aioNuResult(),userResponse.asoDeResult(),jwtToken, refreshToken);
+            var jwtToken = "";
+            var refreshToken = "";
+            
+            if (userResponse.aioNuResult() == 1) {
+                jwtToken = jwtService.generateToken(userResponse);
+                refreshToken = jwtService.generateRefreshToken(userResponse);
+            }//else{
+
+                //throw new ResponseStatusException(HttpStatus.FORBIDDEN, userResponse.asoDeResult());
+            //}
+
+            return new TokenResponse(200,userResponse.aioNuResult(),userResponse.asoDeResult(),jwtToken, refreshToken);
+        }catch(ResponseStatusException e){
+            throw new CustomException("Error al validar el login", e);
+        }
+                
     }
 
     @Override
